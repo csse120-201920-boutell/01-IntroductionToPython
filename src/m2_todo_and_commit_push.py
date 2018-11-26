96964c19c8702bc8ee38f48a1037f3af87e23c14
Matt Boutell <boutell@gmail.com>
2018-11-26 08:48:29 -0500

    Done

--- src/m2_todo_and_commit_push.py
+++ src/m2_todo_and_commit_push.py
@@ -1,5 +1,6 @@
-print('Hello, World')
-print("Foo")
+print("Hello, World")
+print("Welcome to CSSE120 from Dr. Matt")
+print(3607 * 34227)
 ###############################################################################
 #
 #   This line is a COMMENT -- a note to human readers of this file.
@@ -57,7 +58,7 @@ print("Foo")
 
 ###############################################################################
 #
-# TODO: 3.
+# DONE: 3.
 #   Add another   print   statement below the current Line 1 above.
 #   It should print any string that you want (but keep it G-rated!)
 #
@@ -70,7 +71,7 @@ print("Foo")
 
 ###############################################################################
 #
-# TODO: 4.
+# DONE: 4.
 #   Add yet another   print   statement.
 #   This one should print the *product* of 3,607 and 34,227.
 #   Let the computer do the arithmetic for you (no calculators!).
